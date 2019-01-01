68c75a8d89de7302a394d3fa09b3ffd9bbed7954
edward1991704 <edward1997104@link.cuhk.edu.hk>
2019-01-02 03:35:12 +0800

    1. change batches size 128

--- model.py
+++ model.py
@@ -173,7 +173,7 @@ class XRAY_model():
             
             # load data
             X_train, _, unlabel_data, _, _ = load_train_data()
-            input_gen = Testing_Generator(X_train + unlabel_data, batch_size = 256,
+            input_gen = Testing_Generator(X_train + unlabel_data, batch_size = 128,
              reshaped_size = self.input_dim[:-1])
 
 
